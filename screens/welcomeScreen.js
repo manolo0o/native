4bb332c7a135993a6f2723eb19edc179814bbc44
manolo0o <147361804+manolo0o@users.noreply.github.com>
2025-02-04 20:51:05 -0500

    feat: :lipstick: Improving first layouts UI

--- screens/welcomeScreen.js
+++ screens/welcomeScreen.js
@@ -32,18 +32,23 @@ export default function WelcomeScreen({ navigation }) {
             <Text style={welcomeStyles.textWhite}> HUB </Text>
             <Text style={welcomeStyles.description}> Your favourite foods in less than 30 minutes! </Text>
         </View>
+        
         <View style={welcomeStyles.ColumContainer}>
 
-          <View style={welcomeStyles.signIn}>
-            <Text style={welcomeStyles.signInText} >sign in with</Text>
+          <View style={welcomeStyles.signInContainer}>
+            <View style={welcomeStyles.line}></View>
+              <View style={welcomeStyles.signIn}>
+                <Text style={welcomeStyles.signInText} >sign in with</Text>
+              </View>
+            <View style={welcomeStyles.line}></View>
           </View>
-      
+
           {/* Buttons Container */}
           <View style={welcomeStyles.ButtonContainer}>
           {/* Facebook button */}
             <TouchableOpacity style={welcomeStyles.buttonFacebook}>
               <Image
-                source={require('../assets/icons/facebookIcon.png')}
+                source={require('../assets/icons/logos--facebook.png')}
                 style={welcomeStyles.facebookIcon}>
               </Image>
               <Text style={welcomeStyles.buttonText}> FACEBOOK </Text>
@@ -51,7 +56,7 @@ export default function WelcomeScreen({ navigation }) {
           {/* Google button */} 
             <TouchableOpacity style={welcomeStyles.buttonGoogle}>
               <Image 
-                source={require('../assets/icons/googleIcon.png')}
+                source={require('../assets/icons/flat-color-icons--google.png')}
                 style={welcomeStyles.googleIcon}>
               </Image>
               <Text style={welcomeStyles.buttonText}> GOOGLE </Text>

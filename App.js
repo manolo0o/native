3b0fbb885fa1d617608bc97495dc1a7f78d76c4b
manolo0o <147361804+manolo0o@users.noreply.github.com>
2025-02-05 00:57:29 -0500

    feat: :white_check_mark: routing the splash screen.

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import SplashScreen from './components/splashScreen';
 import WelcomeScreen from './screens/welcomeScreen';
 import SignInScreen from './screens/signInScreen';
 
@@ -8,11 +9,16 @@ const Stack = createStackNavigator();
 
 export default function App() {
 
-
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName='Welcome'>
+      <Stack.Navigator initialRouteName='Splash'>
         
+        <Stack.Screen 
+          name="Splash"  
+          component={SplashScreen} 
+          options={{ headerShown: false }}
+        />
+
         <Stack.Screen 
           name="Welcome"  
           component={WelcomeScreen} 

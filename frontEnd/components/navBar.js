60c67fa161ca9e0cd2c0607f48f71e20961f1bee
manolo0o <147361804+manolo0o@users.noreply.github.com>
2025-03-15 09:49:17 -0500

    feat: :white_check_mark: navBar component OK

--- frontEnd/components/navBar.js
+++ frontEnd/components/navBar.js
@@ -1,52 +1,55 @@
-import React, { useState } from "react";
+import { useNavigationState } from '@react-navigation/native';
+import React, { useEffect, useState } from "react";
 import { Image, TouchableOpacity, View } from "react-native";
 import navBarStyles from '../componentStyles/navBarStyles.js';
 
-export default function NavBar() {
-  const [homeIcon, setHomeIcon] = useState(require('../assets/icons/homeBlack.png'));
-  const [orderIcon, setOrderIcon] = useState(require('../assets/icons/ordersBlack.png'));
-  const [cartIcon, setCartIcon] = useState(require('../assets/icons/cartBlack.png'));
+export default function NavBar({ navigation }) {
+  const [selectedIcon, setSelectedIcon] = useState('home');
 
-  const changeIcon = (iconType) => {
-    switch (iconType) {
-      case 'home':
-        setHomeIcon(prevIcon =>
-          prevIcon === require('../assets/icons/homeBlack.png') ? require('../assets/icons/homeOrange.png') : require('../assets/icons/homeBlack.png')
-        );
-        break;
-      case 'order':
-        setOrderIcon(prevIcon =>
-          prevIcon === require('../assets/icons/ordersBlack.png') ? require('../assets/icons/ordersOrange.png') : require('../assets/icons/ordersBlack.png')
-        );
-        break;
-      case 'cart':
-        setCartIcon(prevIcon =>
-          prevIcon === require('../assets/icons/cartBlack.png') ? require('../assets/icons/cartOrange.png') : require('../assets/icons/cartBlack.png')
-        );
-        break;
-      default:
-        break;
+  const icons = {
+    home: {
+      default: require('../assets/icons/homeBlack.png'),
+      selected: require('../assets/icons/homeOrange.png')
+    },
+    order: {
+      default: require('../assets/icons/ordersBlack.png'),
+      selected: require('../assets/icons/ordersOrange.png')
+    },
+    cart: {
+      default: require('../assets/icons/cartBlack.png'),
+      selected: require('../assets/icons/cartOrange.png')
     }
+  };
+
+  // Update selected icon based on the current route
+  const currentRoute = useNavigationState(state => state.routes[state.index].name);
+  useEffect(() => {
+    setSelectedIcon(currentRoute.toLowerCase());
+  }, [currentRoute]);
+
+  const handlePress = (iconType, screen) => {
+    setSelectedIcon(iconType);
+    navigation.navigate(screen);
   };
 
   return (
     <View style={navBarStyles.container}>
-      <TouchableOpacity onPress={() => changeIcon('home')}>
+      <TouchableOpacity onPress={() => handlePress('home', 'Home')}>
         <Image
           style={navBarStyles.image}
-          source={homeIcon}
+          source={selectedIcon === 'home' ? icons.home.selected : icons.home.default}
         />
       </TouchableOpacity>
-      <TouchableOpacity onPress={() => changeIcon('order')}>
+      <TouchableOpacity onPress={() => handlePress('order', 'Order')}>
         <Image
           style={navBarStyles.image}
-          source={orderIcon}
+          source={selectedIcon === 'order' ? icons.order.selected : icons.order.default}
         />
       </TouchableOpacity>
-      <TouchableOpacity onPress={() => changeIcon('cart')}>
+      <TouchableOpacity onPress={() => handlePress('cart', 'Cart')}>
         <Image
           style={navBarStyles.image}
-          source={cartIcon}
+          source={selectedIcon === 'cart' ? icons.cart.selected : icons.cart.default}
         />
       </TouchableOpacity>
     </View>

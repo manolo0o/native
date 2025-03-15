71114ae74e52ddf84d203927be3bf199cd417673
manolo0o <147361804+manolo0o@users.noreply.github.com>
2025-03-15 09:48:17 -0500

    feat: :lipstick: style files

--- frontEnd/screenStyles/cartStyles.js
+++ frontEnd/screenStyles/cartStyles.js
@@ -1,7 +1,11 @@
 import { StyleSheet } from "react-native";
 
 const cartScreenStyles = StyleSheet.create({
-
+  container: {
+    flex: 1,
+    justifyContent: 'space-between', // This will push the NavBar to the bottom
+    alignItems: 'center',
+  },
 })
 
 export default cartScreenStyles;
--- frontEnd/screenStyles/orderStyles.js
+++ frontEnd/screenStyles/orderStyles.js
@@ -1,7 +1,11 @@
 import { StyleSheet } from 'react-native';
 
 const orderScreenStyles = StyleSheet.create({
-
+  container: {
+    flex: 1,
+    justifyContent: 'space-between', // This will push the NavBar to the bottom
+    alignItems: 'center',
+  },
 })
 
 export default orderScreenStyles;

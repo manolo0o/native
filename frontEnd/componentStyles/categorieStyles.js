7c3e7d029782b6d63237debe360aa11719b9495a
manolo0o <147361804+manolo0o@users.noreply.github.com>
2025-03-22 09:50:23 -0500

    feat: :lipstick: Slider component partially done

--- frontEnd/componentStyles/categorieStyles.js
+++ frontEnd/componentStyles/categorieStyles.js
@@ -4,14 +4,27 @@ const categorieStyles = StyleSheet.create({
   categorieItem: {
     padding: 10,
     marginLeft: 20,
-    borderColor: 'black',
-    borderWidth: .5, 
+    borderColor: '#FE8000',
+    borderWidth: 1, 
     borderRadius: 11,
   },
   categorieText: {
     fontSize: 12,
     fontFamily: 'Montserrat_700Bold',
   },
+  categorieItemPressed: {
+    backgroundColor: '#FE8000',
+    padding: 10,
+    marginLeft: 20,
+    borderColor: '#FE8000',
+    borderWidth: .5, 
+    borderRadius: 11,
+  },
+  categorieTextPressed: {
+    fontSize: 12,
+    fontFamily: 'Montserrat_700Bold',
+    color: '#fff',
+  }
 });
 
 export default categorieStyles;
--- frontEnd/components/categories_Slider.js
+++ frontEnd/components/categories_Slider.js
@@ -1,10 +1,11 @@
 import React, { useEffect, useState } from "react";
-import { ActivityIndicator, FlatList, Text, View } from "react-native";
+import { ActivityIndicator, FlatList, Text, TouchableOpacity, View } from "react-native";
 import categorieStyles from "../componentStyles/categorieStyles.js";
 
 export default function CategoriesSlider() {
   const [categories, setCategories] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [pressedItemCategory, setPressedItemCategory] = useState(null);
 
   useEffect(() => {
     fetchCategories();
@@ -22,10 +23,23 @@ export default function CategoriesSlider() {
     }
   }
 
+  const handlePress = (category) => {
+    console.log('Pressed item category:', category);
+    setPressedItemCategory(category);
+  };
+
   const categorieItem = ({ item }) => (
-      <View style={categorieStyles.categorieItem} key={item.id ? item.id.toString() : Math.random().toString()}>
-        <Text style={categorieStyles.categorieText}>{item.product__Category}</Text>
-      </View>
+    <TouchableOpacity
+      style={item.product__Category === pressedItemCategory ? categorieStyles.categorieItemPressed : categorieStyles.categorieItem}
+      key={item.product__Category}
+      onPress={() => handlePress(item.product__Category)}
+    >
+      <Text 
+        style={item.product__Category === pressedItemCategory ? categorieStyles.categorieTextPressed : categorieStyles.categorieText} 
+      >
+        {item.product__Category}
+      </Text>
+    </TouchableOpacity>
   );
 
   if (loading) {
@@ -37,8 +51,9 @@ export default function CategoriesSlider() {
       <FlatList
         data={categories}
         renderItem={categorieItem}
-        keyExtractor={item => item.id ? item.id.toString() : Math.random().toString()}
+        keyExtractor={item => item.product__Category}
         horizontal
+        showsHorizontalScrollIndicator={false}
         style={categorieStyles.Slider}
       />
     </View>

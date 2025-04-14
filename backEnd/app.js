810a1eafc1ae4017b3c59030709cce6b7770c917
manolo0o <147361804+manolo0o@users.noreply.github.com>
2025-04-14 16:21:17 -0500

    feat: :art: Fixing structure of the code

--- backEnd/app.js
+++ backEnd/app.js
@@ -6,15 +6,18 @@ const db = new Connect();
 const app = express();
 const port = process.env.PORT || 3000;
 
-//routes imports
-const productRoute = require('./routes/productRoute.js');
-
-// api routes
-app.use('/products', productRoute);
-
+//middlewares   
 app.use(cors());
 app.use(express.json());
 
+//routes imports
+const productRoute = require('./routes/productRoute.js');
+const authRoutes = require('./routes/authRoutes.js');
+
+// api routes
+app.use('/products', productRoute);
+app.use('/auth', authRoutes);
+
 app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
+  console.log(`Servidor corriendo en http://localhost:${port}`);
 });
